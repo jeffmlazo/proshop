f2ca38c44b9790034070be6f7c7bb78591e52bd0
Jeff Ray <jeffmlazo@yahoo.com>
2023-06-27 07:34:14 +0800

    Additional commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,18 +8,20 @@ import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
-            <Route path="/product/:id" element={<ProductScreen />} />
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 

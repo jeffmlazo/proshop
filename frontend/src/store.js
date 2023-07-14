db9d69cc6e25be3b6cd2983440a970f09f6f2873
Jeff Ray <jeffmlazo@yahoo.com>
2023-07-14 08:58:55 +0800

    Add upload image functionality for product edit page Add productUpdateReducer function

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,6 +10,7 @@ import {
   productDetailsReducer,
   productDeleteReducer,
   productCreateReducer,
+  productUpdateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -34,6 +35,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,

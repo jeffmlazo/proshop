48f4440a9703e7fdb3584f22b0eebb15da3c9f31
Jeff Ray <jeffmlazo@yahoo.com>
2023-08-06 06:08:49 +0800

    Remove HashRouter to BrowserRouter

--- backend/settings.py
+++ backend/settings.py
@@ -135,19 +135,20 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-    # Live database server settings
+    # Local database server settings
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # Live database server settings
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.environ.get('DB_NAME'),
+    #     "USER": os.environ.get('DB_USER'),
+    #     "PASSWORD": os.environ.get('DB_PASSWORD'),
+    #     "HOST": os.environ.get('DB_HOST'),
+    #     "PORT": os.environ.get('DB_PORT'),
+    # }
 }
 
 

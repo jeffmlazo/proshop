56ee44662a074c52389927edd650c0c6f6019418
Jeff Ray <jeffmlazo@yahoo.com>
2023-08-07 08:38:17 +0800

    Update vercel settings

--- backend/settings.py
+++ backend/settings.py
@@ -199,18 +199,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_URL = "images/"
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "frontend/build/static",
 ]
 
-MEDIA_ROOT = BASE_DIR / "static/images"
-
+STATIC_URL = "static/"
 # Needed this join format for Vercel host
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+
+# MEDIA_URL = "images/"
+MEDIA_URLS = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_ROOT = BASE_DIR / "static/images"
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
